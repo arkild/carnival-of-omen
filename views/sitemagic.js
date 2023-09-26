538cb70ba8f4bebbce26c56fb104fc6904283256
Arkild <koray.semenuik@gmail.com>
2023-09-26 17:42:48 -0600

    added a new .js file that will handle events on pages

---
+++ views/sitemagic.js
@@ -0,0 +1,11 @@
+//This is going to be the catch-all for my scripts that I will run on multiple views files. There's a good chance that hardly any of these will intersect with each other, so I'll just make sure they're all in separate clusters. As long as they're all attached to event listeners, the scripts won't fire off here and cause any errors.
+
+//Possible association - I need to make my creation sites associate options they select with the things I want them to make. Whether or not that is achieved through Javascript still remains to be seen. If it is, I would either need to link these .js files together or have the script done on the side that the current .js file will complete.
+
+//"Magic Word" plan: Have my creation and destruction routes require a "Magic Word" that someone has to type out before the magic can occur.
+
+//Admin button = Add an event listener on my entry-details page that will make an admin button disappear and the edit/delete buttons appear on click.
+
+
+
+//Goal for the interactivity here - set up potential callback functions that, when completed, will then initiate the routes on the controllers files. Either have the callback functions pass something back to the .ejs file that initiates the route, or have something initiate the route directly from this file. Either way, tricky, we haven't done it before, and the fact that I hadn't planned this ahead of time can potentially put a wedge in things here. Luckily, we have several days to knock it out if I can't get it just right by tomorrow. We'll just avoid the magic words and timed pages for "stretch".
